3b711237512e6adfa371aff20663d27f7766173c
Dharstec <info@dharstec.com>
2023-04-04 11:33:47 +0530

    img check

--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const db = require("./config/db");
-
+const path = require('path')
+// const img = require('./public/files/admin-myFile-1679649851321.mp4')
 app.use(cors());
 app.use(bodyParser.json({ limit: "500mb" }));
 
-// app.use(express.static(__dirname + '/public'));
+// app.use(express.static('public/files'));
+app.use(express.static(path.join(__dirname, '../public/files')));
+// app.use(express.static(`${__dirname} /public/files`));
+
+
 
 require("./baseRouter")(app);
 
